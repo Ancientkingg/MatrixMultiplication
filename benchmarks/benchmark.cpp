487bd93b7c6f9fc1761e3d51981bd8763674c91e
Ancientkingg <samuelmbruin@yahoo.com>
2023-06-14 19:31:47 +0200

    Remove tests and benchmarks of removed implementation

--- benchmarks/benchmark.cpp
+++ benchmarks/benchmark.cpp
@@ -37,22 +37,4 @@ static void BM_naive_array_matrix_mul(benchmark::State& state) {
 
 BENCHMARK(BM_naive_array_matrix_mul)->Arg(3)->Arg(10)->Arg(100)->Arg(1000);
 
-
-static void BM_cache_optimized_array_matrix_mul(benchmark::State& state) {
-	std::unique_ptr<int_fast64_t[]> A = std::make_unique<int_fast64_t[]>(state.range(0) * state.range(0));
-	std::unique_ptr<int_fast64_t[]> B = std::make_unique<int_fast64_t[]>(state.range(0) * state.range(0));
-
-	// Generate matrices with random elements of size `size`
-	for (size_t i = 0; i < state.range(0) * state.range(0); i++) {
-		A[i] = rand();
-		B[i] = rand();
-	}
-
-	for (auto _ : state) {
-		benchmark::DoNotOptimize(cache_optimized_array_matrix_mul(A.get(), B.get(), state.range(0)));
-	}
-}
-
-BENCHMARK(BM_naive_array_matrix_mul)->Arg(3)->Arg(10)->Arg(100)->Arg(1000);
-
 BENCHMARK_MAIN();
